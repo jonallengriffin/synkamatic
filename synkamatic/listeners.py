c0fc16cb6a49c7ec8d5c9454bcd4cd85c975edbc
Jeff Hammel <jhammel@mozilla.com>
2012-10-03 14:10:26 -0700

    oh git...

---
+++ synkamatic/listeners.py
@@ -0,0 +1,9 @@
+"""
+listeners for synkamatic
+"""
+
+class Listener(object):
+    """abstract base class for synkamatic listeners"""
+
+class GithubPullRequestListener(Listener):
+    """listen to github pull requests"""
